b0a83ea582a5e2240112738429a9eb022633e73d
David Delarosa <xdavidel@gmail.com>
2019-02-26 07:56:36 +0200

    fish vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
+fish_vi_key_bindings
+
 alias l='ls'
-alias dotconf='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias dotconf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
