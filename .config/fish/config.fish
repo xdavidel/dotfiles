095ecba01ffc70560a586b8d7150156ffededede
David Delarosa <xdavidel@gmail.com>
2019-03-03 15:49:02 +0200

    Change fish config to always find git package

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,5 @@ fish_vi_key_bindings
 export EDITOR=vim
 
 alias l='ls'
-alias dotconf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotconf='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
