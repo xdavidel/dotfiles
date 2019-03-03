4b1bbb0997aec45347dab6aaa8b34a7bb6ab88fa
David Delarosa <xdavidel@gmail.com>
2019-03-03 20:44:54 +0200

    Modify to support pkg package manager

--- .config/fish/functions/packs.fish
+++ .config/fish/functions/packs.fish
@@ -3,5 +3,8 @@ function packs -d "Shows installed packages with information using fzf"
 		if type -q pacman
 			pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
 		end
+		if type -q pkg
+			pkg list-installed | cut -d/ -f1 | fzf --preview 'pkg show {}' --layout=reverse --bind 'enter:execute(pkg show {} | less)'
+		end
 	end	
 end
