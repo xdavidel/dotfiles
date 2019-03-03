35c5506543c297cbb929b1acf3be20d938acbf38
David Delarosa <xdavidel@gmail.com>
2019-03-03 15:50:55 +0200

    Only support pacman

--- .config/fish/functions/packs.fish
+++ .config/fish/functions/packs.fish
@@ -1,5 +1,7 @@
 function packs -d "Shows installed packages with information using fzf"
 	if type -q fzf
-		pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
+		if type -q pacman
+			pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
+		end
 	end	
 end
