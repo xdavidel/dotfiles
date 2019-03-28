7f9494bfb6f8146ab46df007ce6e1c76c0f6d589
David Delarosa <xdavidel@gmail.com>
2019-03-28 14:34:13 +0200

    lsd aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,14 @@ fish_vi_key_bindings
 
 export EDITOR=vim
 
-alias l='ls'
+if type -q lsd
+	alias l='lsd -l'
+	alias ls='lsd'
+	alias ll='lsd -la'
+	alias la='lsd -a'
+else
+	alias l='ls -l'
+end
+
 alias dotconf='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
