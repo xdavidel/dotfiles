16e294728cf31942e268aece8e6fcad2445fe6a8
David Delarosa <xdavidel@gmail.com>
2020-01-01 09:33:17 +0200

    fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,29 @@ export EDITOR=vim
 
 alias dotconf='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias q='exit'
+alias ka="killall"
+alias calcurse="calcurse -D ~/.config/calcurse"
+alias f="$FILE"
+alias e="$EDITOR"
+alias v="$EDITOR"
+alias q="exit"
+alias x="sxiv -ft *"
+alias ytv="youtube-dl --add-metadata -i -o '%(upload_date)s-%(title)s.%(ext)s'"
+alias yta="youtube-dl -xi --audio-format mp3"
+alias l="ls -gGhF --color=auto --group-directories-first"
+alias ls="ls -F --color=auto --group-directories-first"
+alias la="ls -AF --color=auto --group-directories-first"
+alias ll="ls -gGAFh --color=auto --group-directories-first"
+alias dotconf="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+alias grep="grep --color=auto"
+alias ffmpeg="ffmpeg -hide_banner"
+
+if type -q nvim
+	alias vim="nvim"
+	alias vimdiff="nvim -d" # Use neovim for vim if possible
+end
+
+function vf
+	fzf | xargs -r - I % $EDITOR %
+end
+
