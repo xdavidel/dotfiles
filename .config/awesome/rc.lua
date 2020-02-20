be8f49ebded39fac35d7aedec195da038f6fb1a4
David Delarosa <xdavidel@gmail.com>
2020-02-20 12:35:47 +0200

    remove test action

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -417,8 +417,8 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 2, function () naughty.notify({ preset = naughty.config.presets.critical, title = "Test", text = tags[3].name }) end)
+    awful.button({ }, 3, function () mymainmenu:toggle() end)
+    -- awful.button({ }, 2, function () naughty.notify({ preset = naughty.config.presets.critical, title = "Test", text = tags[3].name }) end)
     -- awful.button({ }, 4, awful.tag.viewnext),
     -- awful.button({ }, 5, awful.tag.viewprev)
 ))
