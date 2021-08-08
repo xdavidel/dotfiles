e8b306f697b83640bc9aba76f8292484c1f8e089
David Delarosa <xdavidel@gmail.com>
2021-08-08 15:57:01 +0300

    more mappings

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,71 +1,82 @@
+local map = vim.api.nvim_set_keymap
+local cmd = vim.cmd
+
 -- better window movement
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { silent = true })
+map("n", "<C-h>", "<C-w>h", { silent = true })
+map("n", "<C-j>", "<C-w>j", { silent = true })
+map("n", "<C-k>", "<C-w>k", { silent = true })
+map("n", "<C-l>", "<C-w>l", { silent = true })
 
 -- Terminal window navigation
-vim.api.nvim_set_keymap("t", "<C-h>", "<C-\\<C-N><C-w>h", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("t", "<C-j>", "<C-\\<C-N><C-w>j", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("t", "<C-k>", "<C-\\<C-N><C-w>k", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("t", "<C-l>", "<C-\\<C-N><C-w>l", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\<C-n>", {silent = true, noremap = true})
+map("t", "<C-h>", "<C-\\<C-N><C-w>h", {silent = true, noremap = true})
+map("t", "<C-j>", "<C-\\<C-N><C-w>j", {silent = true, noremap = true})
+map("t", "<C-k>", "<C-\\<C-N><C-w>k", {silent = true, noremap = true})
+map("t", "<C-l>", "<C-\\<C-N><C-w>l", {silent = true, noremap = true})
+map("t", "<Esc>", "<C-\\<C-n>", {silent = true, noremap = true})
 
 -- resize with arrows
-vim.api.nvim_set_keymap("n", "<C-A-Up>", ":resize -2<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-A-Down>", ":resize +2<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-A-Left>", ":vertical resize -2<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<C-A-Right>", ":vertical resize +2<CR>", { silent = true })
+map("n", "<C-A-Up>", ":resize -2<CR>", { silent = true })
+map("n", "<C-A-Down>", ":resize +2<CR>", { silent = true })
+map("n", "<C-A-Left>", ":vertical resize -2<CR>", { silent = true })
+map("n", "<C-A-Right>", ":vertical resize +2<CR>", { silent = true })
 
 -- better indenting
-vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
+map("v", "<", "<gv", { noremap = true, silent = true })
+map("v", ">", ">gv", { noremap = true, silent = true })
 
 -- Tab switch buffer
-vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
+map("n", "tn", ":tabnext<CR>", { noremap = true, silent = true })
+map("n", "tp", ":tabprevious<CR>", { noremap = true, silent = true })
+map("n", "tx", ":tabclose<CR>", { noremap = true, silent = true })
+map("n", "tc", ":tabnew<CR>", { noremap = true, silent = true })
+map("n", "to", ":tabnew ", { noremap = true, silent = true })
 
 -- Move current line / block with Alt-Up/Down ala vscode.
-vim.api.nvim_set_keymap("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<A-Down>", ":m '>+1<CR>gv-gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("x", "<A-Up>", ":m '<-2<CR>gv-gv", { noremap = true, silent = true })
+map("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+map("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
+map("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
+map("x", "<A-Down>", ":m '>+1<CR>gv-gv", { noremap = true, silent = true })
+map("x", "<A-Up>", ":m '<-2<CR>gv-gv", { noremap = true, silent = true })
 
 -- QuickFix
-vim.api.nvim_set_keymap("n", "]q", ":cnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "[q", ":cprev<CR>", { noremap = true, silent = true })
+map("n", "]q", ":cnext<CR>", { noremap = true, silent = true })
+map("n", "[q", ":cprev<CR>", { noremap = true, silent = true })
 
 -- Capital Y should behave like this
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true, silent = true })
+map("n", "Y", "y$", { noremap = true, silent = true })
+
+-- Keep it centered
+map("n", "n", "nzzzv", { noremap = true, silent = true })
+map("n", "N", "Nzzzv", { noremap = true, silent = true })
+map("n", "J", "mzJ'z", { noremap = true, silent = true })
 
 -- Delete / Change until '_'
-vim.api.nvim_set_keymap("n", "dc", "td_", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "cd", "ct_", { noremap = true, silent = true })
+map("n", "dc", "td_", { noremap = true, silent = true })
+map("n", "cd", "ct_", { noremap = true, silent = true })
 
 -- Wrap text
-vim.api.nvim_set_keymap("n", "<M-z>", ":set wrap!<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<M-z>", "<Esc>:set wrap!<CR>a", { noremap = true, silent = true })
+map("n", "<M-z>", ":set wrap!<CR>", { noremap = true, silent = true })
+map("i", "<M-z>", "<Esc>:set wrap!<CR>a", { noremap = true, silent = true })
 
 -- Mark position before search
-vim.api.nvim_set_keymap("n", "/", "ms/", { noremap = true, silent = true })
+map("n", "/", "ms/", { noremap = true, silent = true })
 
 -- Delete matches
-vim.api.nvim_set_keymap("n", "dm", ":%s/<c-r>///g<CR>", { noremap = false, silent = true })
+map("n", "dm", ":%s/<c-r>///g<CR>", { noremap = false, silent = true })
 
 -- Change matches
-vim.api.nvim_set_keymap("n", "cm", ":%s/<c-r>///g<Left><Left>", { noremap = false, silent = true })
+map("n", "cm", ":%s/<c-r>///g<Left><Left>", { noremap = false, silent = true })
 
 -- Replace all is aliased to S
-vim.api.nvim_set_keymap("n", "S", ":%s//g<Left><Left>", { noremap = true, silent = true })
+map("n", "S", ":%s//g<Left><Left>", { noremap = true, silent = true })
 
 -- Better nav for omnicomplete
-vim.cmd 'inoremap <expr> <c-j> ("\\<C-n>")'
-vim.cmd 'inoremap <expr> <c-k> ("\\<C-p>")'
+cmd 'inoremap <expr> <c-j> ("\\<C-n>")'
+cmd 'inoremap <expr> <c-k> ("\\<C-p>")'
 
-vim.cmd 'vnoremap p "0p'
-vim.cmd 'vnoremap P "0P'
+cmd 'vnoremap p "0p'
+cmd 'vnoremap P "0P'
 
 -- Toggle the QuickFix window
-vim.api.nvim_set_keymap("", "<C-q>", ":call QuickFixToggle()<CR>", { noremap = true, silent = true })
+map("", "<C-q>", ":call QuickFixToggle()<CR>", { noremap = true, silent = true })
